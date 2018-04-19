32eb6465c46779690c3e3cdcddca5e5edc0f5fb0
liukui17 <liukui780@gmail.com>
2018-04-19 16:07:44 -0700

    slightly more descriptive readme. gan skeleton

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # ERLGAN
 Evolutionary Reinforcement Learned GANs
+
+This our final project for the Deep Reinforcement Learning topics course offered Spring 2018.
---
+++ gan.py
@@ -0,0 +1,17 @@
+import numpy as np
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
+import torch.utils.data as data
+
+class GAN(nn.Module):
+
+	def __init__(self):
+		super(Generator, self).__init__()
+		# TODO
+
+	def forward_generator(self):
+		# TODO
+
+	def forward_discriminator(self):
+		# TODO
